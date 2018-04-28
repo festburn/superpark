b7049aa408751dbd8f22ee05cde218144069883a
Yemi Ilupeju <festburn@gmail.com>
2018-04-28 11:16:01 -0600

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -20,3 +20,89 @@ var firebaseSpotCollection = database.ref().child('spots');
 var firebaseDriverCollection = database.ref().child('drivers');
 var firebaseOwnerCollection = database.ref().child('owners');
 var firebaseBookingCollection = database.ref().child('bookings');
+
+//SPOT functions
+function createSpot(image,address,stall,description,price)
+{
+    
+};
+
+function readSpot()
+{
+    
+};
+
+function updateSpot(image,address,stall,description,price)
+{
+    
+};
+
+function deleteSpot(spotId)
+{
+    
+};
+
+//DRIVER functions
+function createDriver(firstName, lastName, carMake, licensePlate)
+{
+    
+};
+
+function readDriver()
+{
+    
+};
+
+function updateDriver(firstName, lastName, carMake, licensePlate)
+{
+    
+};
+
+function deleteDriver(driverId)
+{
+    
+};
+
+//OWNER functions
+function createOwner(firstName, lastName, phone, email, proof, spots)
+{
+    
+};
+
+function readOwner()
+{
+    
+};
+
+function updateOwner(firstName, lastName, phone, email, proof, spots)
+{
+    
+};
+
+function deleteOwner(ownerId)
+{
+    
+};
+
+
+//BOOKING functions
+function createBooking(ownerId, driverId, spotId, startDate, endDate, cost, paidFlag)
+{
+    
+};
+
+function readBooking()
+{
+    
+};
+
+function updateBooking(ownerId, driverId, spotId, startDate, endDate, cost, paidFlag)
+{
+    
+};
+
+function deleteBooking(bookingId)
+{
+    
+};
+
